afb4908f7f725432a4b0e4058b06ce09aba4560f
keerthana121102 <101855404+keerthana121102@users.noreply.github.com>
2023-02-08 19:37:17 +0530

    restaurant_landing_page

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {SubHeading} from '../../components';
+import {images} from '../../constants';
+import './Chef.css';
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+       <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+        <p className="p__opensans">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
